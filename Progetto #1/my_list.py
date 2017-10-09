dc1d5d4823f6e3dd04c17bcda0faefa631b13374
Mario Cantalupo <mariocantalupo77@gmail.com>
2017-10-09 14:39:22 +0200

    Definizione classe MyList - test first commit and push

--- Progetto #1/my_list.py
+++ Progetto #1/my_list.py
@@ -3,28 +3,30 @@ class MyList:
 
     class Node:
         def __init__(self):
-            self._element = None
             self._left = None
+            self._element = None
             self._right = None
 
 
     def __init__(self):
-        self._size = 0
-
+        self._root = None           #puntatore al primo nodo
+        self._size = 0              #dimensione della lista
 
     def append(self,x):
 
         new = self.Node() #creazione nodo
-        new._data = x     #assegno valore al nodo
+        new._element = x     #assegno valore x al nodo
 
-        if(self == None):                     #lista vuota
-            self = new
+        if(len(self)==0):                     #lista vuota
+            self._root = new
         else:                                 #lista con almeno un elemento
-            last = self.Node                  #scorro gli elementi della lista fino alla coda
+            last = self._root                  #scorro gli elementi della lista fino alla coda
             while(last._right != None):
-                head = last._right
-            new._left = last                  #aggiorno il link sisistro dell'ultimo nodo
+                last = last._right
+            new._left = last                  #aggiorno il link sinistro dell'ultimo nodo
+            last._right = new
 
+        self._size += 1
 
     def insert(self,i,x):           #aggiungere eccezione se i non è compreso tra 0 e la dimensione della lista?
 
@@ -32,6 +34,8 @@ class MyList:
             new = self.Node()       # creazione nodo
             new._data = x           # assegno valore al nodo
             new._right = self
+            self._root._left = new
+            self._root = new
         elif(i==self._size):        #inserimento in coda (append)
             self.append(x)
         elif (i>0 and i<self._size):   #inserimento all'interno della lista
@@ -44,3 +48,5 @@ class MyList:
             new._right = node._right
             node._right = new
             new._left = node._right
+    def __len__(self):
+        return self._size
