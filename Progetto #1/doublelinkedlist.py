6feabe39e53e44f058e78b6ea028f3a6ba08ac2f
Francesco <f.falanga7@studenti.unisa.it>
2017-10-10 14:34:05 +0200

    Added interface doublelinkedlist to implement

---
+++ Progetto #1/doublelinkedlist.py
@@ -0,0 +1,26 @@
+ class DoubleLinkedList:
+     def append(self,x):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+     def extend(self,iterable):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+     def insert(self,i,x):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+     def remove(self,x):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+     def pop(self):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+     def clear(self):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+     def index(self):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+     def count(self,x):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+     def sort(self,key=None,reverse=False):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+     def reverse(self):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+     def copy(self):
+      raise NotImplementedError('deve essere implementato dalla sottoclasse.')
+
+
+
