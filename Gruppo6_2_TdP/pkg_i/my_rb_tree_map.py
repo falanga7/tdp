b13cb9849cae05dcbfb88e6598ace81a258bd051
Francesco <f.falanga7@studenti.unisa.it>
2017-11-03 19:00:34 +0100

    Parziale implementazione dell'esercizio 4, fusion.

--- Gruppo6_2_TdP/pkg_i/my_rb_tree_map.py
+++ Gruppo6_2_TdP/pkg_i/my_rb_tree_map.py
@@ -2,6 +2,59 @@ from ..TdP_collections.map.red_black_tree import RedBlackTreeMap
 
 
 class MyRBTreeMap(RedBlackTreeMap):
+
+    def _black_depth(self):
+        """Return black depth of RB Tree."""
+        walk = self.root()
+        self._black_depth = 1
+        while self.left(walk) is not None:  # keep walking left
+            walk = self.left(walk)
+            if not super()._is_red(walk):
+                self._black_depth += 1
+        return self._black_depth
+
+    def _find_black_parent(self, bd1):
+        """Return black position of specified depth."""
+        bd = self._black_depth()
+        if bd < bd1:
+            raise ValueError("L'altezza specificata è più grande di quella dell'albero.")
+        walk = self.root()
+        d_bd = bd - bd1 -1
+        while d_bd != 0:
+            walk = self.right(walk)
+            if not super()._is_red(walk):
+                d_bd-= 1
+        return walk
+
+    def fusion(self, t1):
+        maxt = self.last()                               # O(logn)
+        mint1 = t1.first()                               # O(logm)
+        if mint1 < maxt:
+            raise ValueError("Le chiavi dell'albero passato non sono maggiori delle chiavi di quest'albero.")
+        bd_t = self._black_depth()                        # O(logn)
+        bd_t1 = t1._black_depth()                         # O(logm)
+        new_mint1 = self._make_position(mint1)
+        del t1[mint1]                                     # O(logm)
+
+        if bd_t == bd_t1:
+            t_size = self._size
+            t1_size = t1._size
+            self._attach(new_mint1, self, t1)
+            self._add_root(new_mint1.element())
+            self._set_black(self.root())
+            self._size = t_size + t1_size + 1
+        elif bd_t > bd_t1:
+            p = self._find_black_parent(bd_t1)
+            mint1_left = p.right()
+            self._add_right(p, new_mint1.element())
+            self._add_left(new_mint1, mint1_left.element())
+            self._add_right(new_mint1, )
+
+
+
+
+
+
     def split(self, k):
         node = self._validate(self.find_position(k))
         T1 = node._left
