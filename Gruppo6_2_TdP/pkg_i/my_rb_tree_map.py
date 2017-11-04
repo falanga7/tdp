ae3be1f4fc63d44e34368ab84981e4a092b9b5b7
Francesco <f.falanga7@studenti.unisa.it>
2017-11-04 19:17:18 +0100

    Fusion implementato in modo completo, manca split

--- Gruppo6_2_TdP/pkg_i/my_rb_tree_map.py
+++ Gruppo6_2_TdP/pkg_i/my_rb_tree_map.py
@@ -6,24 +6,37 @@ class MyRBTreeMap(RedBlackTreeMap):
     def _black_depth(self):
         """Return black depth of RB Tree."""
         walk = self.root()
-        self._black_depth = 1
+        black_depth = 1
         while self.left(walk) is not None:  # keep walking left
             walk = self.left(walk)
             if not super()._is_red(walk):
-                self._black_depth += 1
-        return self._black_depth
+                black_depth += 1
+        return black_depth
 
-    def _find_black_parent(self, bd1):
-        """Return black position of specified depth."""
+    def _find_black_parent_right(self, bd1):
+        """Return black position of specified depth on right of root."""
         bd = self._black_depth()
         if bd < bd1:
-            raise ValueError("L'altezza specificata è più grande di quella dell'albero.")
+            raise ValueError("La profondità specificata è più grande di quella dell'albero.")
         walk = self.root()
-        d_bd = bd - bd1 -1
+        d_bd = bd - bd1 - 1
         while d_bd != 0:
             walk = self.right(walk)
             if not super()._is_red(walk):
-                d_bd-= 1
+                d_bd -= 1
+        return walk
+
+    def _find_black_parent_left(self, bd1):
+        """Return black position of specified depth on left of root."""
+        bd = self._black_depth()
+        if bd < bd1:
+            raise ValueError("La profondità specificata è più grande di quella dell'albero.")
+        walk = self.root()
+        d_bd = bd - bd1 - 1
+        while d_bd != 0:
+            walk = self.left(walk)
+            if not super()._is_red(walk):
+                d_bd -= 1
         return walk
 
     def fusion(self, t1):
@@ -33,42 +46,53 @@ class MyRBTreeMap(RedBlackTreeMap):
             raise ValueError("Le chiavi dell'albero passato non sono maggiori delle chiavi di quest'albero.")
         bd_t = self._black_depth()                        # O(logn)
         bd_t1 = t1._black_depth()                         # O(logm)
-        new_mint1 = self._make_position(mint1)
+        len1 = len(self)
+        len2 = len(t1)
+        self._size = len1 + len2
+        node = self._validate(mint1)
         del t1[mint1]                                     # O(logm)
+        node._parent = None
 
         if bd_t == bd_t1:
-            t_size = self._size
-            t1_size = t1._size
-            self._attach(new_mint1, self, t1)
-            self._add_root(new_mint1.element())
-            self._set_black(self.root())
-            self._size = t_size + t1_size + 1
+            self._root._parent = node
+            node._left = self._root
+            self._root = node
+            t1._root._parent = node
+            node._right = t1._root
+
         elif bd_t > bd_t1:
-            p = self._find_black_parent(bd_t1)
-            mint1_left = p.right()
-            self._add_right(p, new_mint1.element())
-            self._add_left(new_mint1, mint1_left.element())
-            self._add_right(new_mint1, )
-
-
-
-
-
+            bp = self._validate(self._find_black_parent_right(bd_t1))           # O(log(m))
+            node._left = bp._right
+            bp._right = node
+            node._parent = bp
+            node._right = t1._root
+            node._red = True
+            child = self._make_position(node._left)
+            self._rebalance_insert(child)               # O(logn)
+        else:
+            bp = t1._validate(t1._find_black_parent_left(bd_t))                 # O(log(n))
+            node._right = bp._left
+            bp._left = node
+            node._parent = bp
+            node._left = self._root
+            node._red = True
+            self._root = t1._root
+            child = self._make_position(node._right)
+            self._rebalance_insert(child)               # O(logm)
+        t1._root = None
+        t1._size = 0
 
     def split(self, k):
-        node = self._validate(self.find_position(k))
-        T1 = node._left
-        T2 = node._right
-        node._parent._left = None
-        node._left = None
-        node._right = None
-        if self.is_root(node):
-            return T1, T2
-        elif node == node._parent._left:
-            T2._attach(T2.last(), None, node._parent)
-        else:
-            T1._attach(T1.last(), node._parent, None)
-        node._parent = node
-        return T1, T2
+        p = self.find_position(k)                       # O(logn)
+        if p is None or p.key() != k:
+            raise ValueError("La chiave k non appartiene all'albero.")
+        node = self.validate(p)
+        t1 = node._left
+        t2 = node._right
+
+
+        self._root = None
+        self._size = 0
+        return t1, t2
 
 
