533fbda3303ea4250ffeae5f3d62b9b2294b7028
Francesco <f.falanga7@studenti.unisa.it>
2017-11-25 12:37:22 +0100

    Alcuni test fatti sulla seconda parte, vanno finiti.

--- Gruppo6_3_TdP/count_kmp.py
+++ Gruppo6_3_TdP/count_kmp.py
@@ -2,12 +2,18 @@ from TdP_collections.text.find_kmp import *
 
 
 def count_kmp(t, p):
+    if not isinstance(t, str):
+        raise TypeError("Non è stata specificata una stringa in cui ricercare il pattern ma un altro tipo.")
+    if not isinstance(p, str):
+        raise TypeError("Il pattern deve essere una stringa.")
+
     m = len(p)
+    if m == 0:
+        return 0
     j = find_kmp(t, p)
     count = 0
     while j != -1:
         count += 1
-        t = t[j+m:]
+        t = t[j + m:]
         j = find_kmp(t[j:], p)
     return count
-
--- Gruppo6_3_TdP/verifica2.py
+++ Gruppo6_3_TdP/verifica2.py
@@ -7,3 +7,31 @@ print(count)
 print("Secondo test:count_kmp(\"abababa\",\"aba\")=")
 count = count_kmp("abababa", "aba")
 print(count)
+
+# print("Terzo test:count_kmp(22, 33)=")
+# count = count_kmp(22, 33)
+# print(count)
+
+# print("Quarto test:count_kmp(\"test\", 33)=")
+# count = count_kmp("test", 33)
+# print(count)
+
+print("Quinto test:count_kmp(\"aba\",\"abababa\")=")
+count = count_kmp("aba", "abababa")
+print(count)
+
+print("Sesto test:count_kmp(\"aba\",\"aba\")=")
+count = count_kmp("aba", "aba")
+print(count)
+
+print("Settimo test:count_kmp(\"\",\"aba\")=")
+count = count_kmp("", "aba")
+print(count)
+
+print("Ottavo test:count_kmp(\"\",\"\")=")
+count = count_kmp("", "")
+print(count)
+
+print("Nono test:count_kmp(\"aba\",\"\")=")
+count = count_kmp("aba", "")
+print(count)
