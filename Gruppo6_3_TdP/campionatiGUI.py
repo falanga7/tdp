0b4153705e54b37bde3d11138f9bb4c3a2f58d43
mariounisa77 <32866091+mariounisa77@users.noreply.github.com>
2017-12-02 13:03:19 +0100

    Modificata classe campionato (aggiunto nome). Modificata campionatiGUI.py ( viene mostrato il nome del campionato selezionato tramite codice)

--- Gruppo6_3_TdP/campionatiGUI.py
+++ Gruppo6_3_TdP/campionatiGUI.py
@@ -7,49 +7,29 @@ from math import sqrt
 from Gruppo6_3_TdP.TdP_collections.hash_table.chain_hash_map import ChainHashMap
 from Gruppo6_3_TdP.TdP_collections.hash_table.probe_hash_map import ProbeHashMap
 from Gruppo6_3_TdP.campionato import Campionato
-from Gruppo6_3_TdP.squadra import Squadra
-from Gruppo6_3_TdP.partita import Partita
 
 
 def dispatcher(file, cl):
     x_workbook = xlrd.open_workbook(file)
     campionati = ProbeHashMap(cap=int(len(cl)/0.5 + 1))
-    for campionatoe in cl:
+    for campionatoe in cl.keys():
         x_sheet = x_workbook.sheet_by_name(campionatoe)
         nrows = x_sheet.nrows
         ns = int((1 + sqrt(1 + 4 * (nrows - 1))) / 2)
         ng = (ns - 1) * 2
         squadre = ChainHashMap(cap=int(ns / 0.9 + 1))
-        giornate = [None] * (ng + 1)
         n = 1
         try:
             while not squadre._n == ns or not n == x_sheet.nrows:
                 home_team = x_sheet.cell(n, 2).value
                 away_team = x_sheet.cell(n, 3).value
-                squadre[home_team] = Squadra(home_team)
-                squadre[away_team] = Squadra(away_team)
+                squadre[home_team] = home_team
+                squadre[away_team] = away_team
                 n += 1
         except IndexError:
-            print("Il campionato in questione non supporta il nostro algoritmo.")
-        n = 0
-        for row_idx in range(1, nrows):
-            home_team = x_sheet._cell(row_idx, 2)
-            away_team = x_sheet._cell(row_idx, 3)
-            partita = Partita(x_sheet._cell(row_idx, 1), home_team, away_team, x_sheet._cell(row_idx, 4),
-                              x_sheet._cell(row_idx, 5), x_sheet._cell(row_idx, 6), x_sheet._cell(row_idx, 7), x_sheet._cell(row_idx, 8),
-                              x_sheet._cell(row_idx, 9))
-            sht = squadre[home_team]
-            sat = squadre[away_team]
-            sht + 1
-            sat + 1
-            if sht.partite == n +1:
-                n += 1
-
-
-
-
-
-        campionato = Campionato(campionatoe, squadre)
+            print(n)
+        print(campionatoe,"_",cl[campionatoe])
+        campionato = Campionato(campionatoe,cl[campionatoe], squadre)
         campionati[campionatoe] = campionato
     return campionati
 
@@ -57,7 +37,8 @@ def dispatcher(file, cl):
 def oncschange(index, value, op):
     for squadraR in squadreT.get_children():
         squadreT.delete(squadraR)
-    squadreT.heading("#0", text='Squadre', anchor='w')
+    squadreT.heading("#0").clear()
+    squadreT.heading("#0", text=campionati[comboC.get()].nome(), anchor='w')
     squadreT.column("#0", anchor="w")
     squadred = campionati[comboC.get()].squadre()
     for squadra in squadred:
@@ -71,8 +52,8 @@ root.title("Campionati")
 root.resizable(0, 0)
 c1 = ttk.Label(root, justify=tk.CENTER, text="Seleziona il campionato:").pack(side="top")
 csv = StringVar()
-oc = ["E0", "SC0", "D1", "SP1", "I1", "F1", "N1", "B1", "P1", "T1", "G1"]
-comboC = ttk.Combobox(root, textvariable=csv, values=oc)
+oc = {"E0":'Premier League', "SC0":'Scottish Premiership', "D1":'Bundesliga', "SP1":'Liga Spagnola', 'I1':'Serie A', "F1":'Ligue 1', "N1":'Eredivise', "B1":'Pro League', "P1":'Primeira Liga', "T1":'Super Lig', "G1":'Souper Ligka Ellada'}
+comboC = ttk.Combobox(root, textvariable=csv, values=list(oc.keys()))
 csv.trace("w", oncschange)
 comboC.pack()
 ac = ttk.Notebook(root)
@@ -83,7 +64,6 @@ ac.add(f2, text='Two')
 ac.pack()
 squadreT = ttk.Treeview(root, height=33)
 campionati = dispatcher("all-euro-data-2016-2017.xls", oc)
-
 
 # codice per interfaccia grafica a schermo intero, premere ESC per uscire
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
--- Gruppo6_3_TdP/campionato.py
+++ Gruppo6_3_TdP/campionato.py
@@ -2,19 +2,25 @@ from Gruppo6_3_TdP.TdP_collections.hash_table.chain_hash_map import ChainHashMap
 
 
 class Campionato:
-    __slots__ = '_codice', '_squadre'
+    __slots__ = '_codice','_nome','_squadre'
 
-    def __init__(self, codice, squadre):
+    def __init__(self, codice, nome, squadre):
 
         if not isinstance(codice, str):
             raise ValueError("Il codice passato non è una stringa.")
+        elif not isinstance(nome, str):
+            raise ValueError("Il nome passato non è una stringa.")
         elif not isinstance(squadre, ChainHashMap):
             raise ValueError("Le squadre devono essere strutturate secondo una ChainHashMap.")
         self._codice = codice
+        self._nome = nome
         self._squadre = squadre
 
     def codice(self):
         return self._codice
+
+    def nome(self):
+        return self._nome
 
     def squadre(self):
         return self._squadre
