e5de34a8d9d5f1cf44c325f63391a64f96bc91ae
Francesco <f.falanga7@studenti.unisa.it>
2017-12-24 19:54:12 +0100

    Implementato in parte min vertex cover

--- Gruppo6_4_TdP/pkg_1/my_graph.py
+++ Gruppo6_4_TdP/pkg_1/my_graph.py
@@ -4,6 +4,49 @@ from TdP_collections.graphs.graph import Graph
 
 class MyGraph(Graph):
 
+    class Vertex(Graph.Vertex):
+        __slots__ = '_cd', '_label'
+
+        def __init__(self, x):
+            """Do not call constructor directly. Use Graph's insert_vertex(x)."""
+            super().__init__(x)
+            self._cd = 0
+            self._label = 0
+
+    def insert_edge(self, u, v, x=None):
+        """Insert and return a new Edge from u to v with auxiliary element x.
+
+        Raise a ValueError if u and v are not vertices of the graph.
+        Raise a ValueError if u and v are already adjacent.
+        """
+        super().insert_edge(u, v, x)
+        u._cd += 1
+
+    def min_vertex_cover(self, k=None, uncov=None, opt=None):
+        if k is None and uncov is None and opt is None:
+            k = self.vertex_count()
+            uncov = self.edge_count()
+            opt = self.edge_count()
+        if k == 0:
+            if uncov < opt:
+                opt = uncov
+        if
+        i = next(self.vertices())
+        i._label = 2
+        k -= 1
+        for edge in self.incident_edges(i):
+            edge.opposite(i)._cd -= 1
+        self.min_vertex_cover(k, uncov-i._cd, opt)
+        i._label = 1
+        k += 1
+        for edge in self.incident_edges(i):
+            edge.opposite(i)._cd += 1
+
+        self.min_vertex_cover(k, uncov, opt)
+        i._label = 0
+
+
+
     def delete_edge(self, u, v):
         """Insert and return a new Edge from u to v with auxiliary element x.
 
@@ -12,10 +55,6 @@ class MyGraph(Graph):
         """
         del self._outgoing[u][v]
         del self._incoming[v][u]
-
-
-    def min_vertex_cover(self):
-        pass
 
     def greedy_vertex_cover(self):
 
