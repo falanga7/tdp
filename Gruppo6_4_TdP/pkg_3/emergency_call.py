e9b77e16d35af36934fcf720a841ef6c65baa132
Francesco <f.falanga7@studenti.unisa.it>
2017-12-31 11:43:14 +0100

    Ordinate alcune cose

--- Gruppo6_4_TdP/pkg_3/emergency_call.py
+++ Gruppo6_4_TdP/pkg_3/emergency_call.py
@@ -1,16 +1,17 @@
 from TdP_collections.graphs.graph import Graph
-from TdP_collections.graphs.shortest_paths import shortest_path_tree
 from TdP_collections.graphs.shortest_paths import shortest_path_lengths
 from TdP_collections.priority_queue.sorted_priority_queue import SortedPriorityQueue
+
+
 def emergency_call(G, pos, v, k):
 
-    if not isinstance(G,Graph):
+    if not isinstance(G, Graph):
         raise ValueError("G deve essere di tipo Graph")
 
-    if not isinstance(pos,dict):
+    if not isinstance(pos, dict):
         raise ValueError("pos deve essere un dizionario")
 
-    if not isinstance(v,Graph.Vertex):
+    if not isinstance(v, Graph.Vertex):
         raise ValueError("v deve essere un oggetto di tipo Vertex")
 
     volanti = {}
@@ -18,10 +19,10 @@ def emergency_call(G, pos, v, k):
     soluzione = list()
 
     for volante, partenza in pos.items():
-        volanti[volante] = shortest_path_lengths(G,partenza)
+        volanti[volante] = shortest_path_lengths(G, partenza)
         for key,value in volanti[volante].items():
-            if key.element() == v.element() and isinstance(value,int):
-                queue.add(value,volante)
+            if key.element() == v.element() and isinstance(value, int):
+                queue.add(value, volante)
                 pass
 
     if not queue.is_empty():
--- Gruppo6_4_TdP/verifica3.py
+++ Gruppo6_4_TdP/verifica3.py
@@ -11,19 +11,19 @@ v4 = grafo1.insert_vertex(4)
 v5 = grafo1.insert_vertex(5)
 v6 = grafo1.insert_vertex(6)
 
-grafo1.insert_edge(v1,v0,1)
-grafo1.insert_edge(v0,v4,4)
-grafo1.insert_edge(v3,v4,3)
-grafo1.insert_edge(v1,v3,10)
-grafo1.insert_edge(v1,v2,1)
-grafo1.insert_edge(v4,v5,2)
-grafo1.insert_edge(v5,v6,1)
-grafo1.insert_edge(v2,v6,1)
-grafo1.insert_edge(v6,v4,1)
+grafo1.insert_edge(v1, v0, 1)
+grafo1.insert_edge(v0, v4, 4)
+grafo1.insert_edge(v3, v4, 3)
+grafo1.insert_edge(v1, v3, 10)
+grafo1.insert_edge(v1, v2, 1)
+grafo1.insert_edge(v4, v5, 2)
+grafo1.insert_edge(v5, v6, 1)
+grafo1.insert_edge(v2, v6, 1)
+grafo1.insert_edge(v6, v4, 1)
 
-polizia = {1:v4,2:v5,3:v6}
+polizia = {1: v4, 2: v5, 3: v6}
 
-volanti = emergency_call(grafo1,polizia,v4,2)
+volanti = emergency_call(grafo1, polizia, v4, 2)
 
 print("Volanti")
 print(volanti)
