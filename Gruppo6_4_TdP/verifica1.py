79ad29742b9da5fa5aea38a2a8866f676c6a7049
Francesco <f.falanga7@studenti.unisa.it>
2018-01-02 21:57:22 +0100

    Fatti ulteriori test su min_vertex_degree

--- Gruppo6_4_TdP/verifica1.py
+++ Gruppo6_4_TdP/verifica1.py
@@ -24,13 +24,16 @@ grafo1.insert_edge(v2, v5)
 grafo1.insert_edge(v5, v6)
 grafo1.insert_edge(v2, v7)
 grafo1.insert_edge(v8, v7)
-# grafo1.insert_edge(v4, v9)
+
+grafo1.insert_edge(v4, v9)
+grafo1.insert_edge(v9, v10)
+grafo1.insert_edge(v9, v6)
 
 grafo1.insert_edge(v3, v7)
 grafo1.insert_edge(v1, v7)
 grafo1.insert_edge(v1, v5)
 grafo1.insert_edge(v3, v5)
-
+print("Primo test:")
 print("GREEDY")
 vertici = grafo1.greedy_vertex_cover()
 for vertice in vertici:
@@ -42,6 +45,7 @@ for vertice in vertici:
     print(vertice)
 
 # secondo test
+
 grafo2 = MyGraph()
 
 v0 = grafo2.insert_vertex(0)
@@ -53,8 +57,48 @@ grafo2.insert_edge(v1, v2)
 grafo2.insert_edge(v2, v0)
 
 vertici = grafo2.min_vertex_cover()
-print(vertici)
+print("Secondo test:")
+print("GREEDY")
+vertici = grafo2.greedy_vertex_cover()
+for vertice in vertici:
+    print(vertice)
+print("MIN")
+vertici = grafo2.min_vertex_cover()
 
+for vertice in vertici:
+    print(vertice)
+
+# terzo test
+
+grafo3 = MyGraph()
+
+v0 = grafo3.insert_vertex(0)
+v1 = grafo3.insert_vertex(1)
+v2 = grafo3.insert_vertex(2)
+v3 = grafo3.insert_vertex(3)
+v4 = grafo3.insert_vertex(4)
+v5 = grafo3.insert_vertex(5)
+v6 = grafo3.insert_vertex(6)
+
+grafo3.insert_edge(v0, v1)
+grafo3.insert_edge(v0, v2)
+grafo3.insert_edge(v1, v2)
+grafo3.insert_edge(v1, v6)
+grafo3.insert_edge(v1, v3)
+grafo3.insert_edge(v3, v5)
+grafo3.insert_edge(v1, v4)
+grafo3.insert_edge(v4, v5)
+
+print("Terzo test:")
+print("GREEDY")
+vertici = grafo3.greedy_vertex_cover()
+for vertice in vertici:
+    print(vertice)
+print("MIN")
+vertici = grafo3.min_vertex_cover()
+
+for vertice in vertici:
+    print(vertice)
 
 #inizializzazione grafi
 n_grafi = randList(51, 60, 1)[0]
